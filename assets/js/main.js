f8739ef1ea948bd006aea09feb3d8f2aa63dcd09
Saksham Gupta <saksham1319@gmail.com>
2020-12-28 13:44:01 +0530

    themes implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,10 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
+
+
+
+
 /*===== ACTIVE AND REMOVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');   
 
@@ -54,8 +58,45 @@ sr.reveal('.skills__img',{delay: 600});
 /*SCROLL WORK*/
 sr.reveal('.work__img',{interval: 200}); 
 
-/*SCROLL CONTACT*/
 
 
+/*===== Theme option =====*/
 
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+	setTheme('light')
+}else{
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'assets/css/styles2.css'
+	}
+
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = 'assets/css/blue.css'
+	}
+
+	if(mode == 'green'){
+		document.getElementById('theme-style').href = 'assets/css/green.css'
+	}
+
+	if(mode == 'purple'){
+		document.getElementById('theme-style').href = 'assets/css/purple.css'
+	}
+
+	localStorage.setItem('theme', mode)
+}
 
